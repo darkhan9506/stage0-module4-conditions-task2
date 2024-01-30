5390d3da27299ec313d0da98f65ce244f9efb029
Darkhan Zhursin <daratechn@gmail.com>
2024-01-29 20:19:58 -0600

    done

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -1,6 +1,11 @@
 package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
+    int res;
     public void printGreatest(int first, int second) {
+        if (first >= second)
+            res = first;
+        else res = second;
+        System.out.println(res);
     }
 }
